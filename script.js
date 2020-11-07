4798bd753cb455ecbfdb067ddbc1fe9f82d346f1
bkkbbkk <62892029+bkkbbkk@users.noreply.github.com>
2020-11-07 17:47:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Фото
 var images = [
 "nada.jpg",
 "kiril.jpg",
@@ -6,8 +7,18 @@ var images = [
 "nada1.jpg",
 "lena.jpg"
 ];
+// Кнопки по смене фото
 var num = 0;
+// Вперд
 function next() {
+	function zagr() {
+	var slider = document.getElementById('slider');
+	slider.animate([
+				{ opacity: 0 },
+                { opacity: 1 },],1000);
+	}
+	setTimeout(zagr, 0)
+
 	var slider = document.getElementById('slider');
 	num++;
 	if (num >= images.length) {
@@ -15,6 +26,7 @@ function next() {
 	}
 	slider.src = images[num];
 }
+// Назад
 function prew() {
 	var slider = document.getElementById('slider');
 	num--;
@@ -23,3 +35,24 @@ function prew() {
 	}
 	slider.src = images[num];
 }
+// Плавная смена фото
+var num = 0;
+function nextt() {
+	var photo1 = document.getElementById('photo1');
+	num++;
+	if (num >= images.length) {
+		num = 0;
+	}
+	photo1.src = images[num];
+}
+setInterval(nextt, 4000);
+// плавное затемнение фото
+function zagr() {
+	var photo2 = document.getElementById('photo1');
+	photo2.animate([ { opacity: 0 },
+                  { opacity: 1 },
+                  { opacity: 1 },
+                  { opacity: 1 },
+                  { opacity: 0 }],4000);
+}
+setInterval(zagr, 4000);
